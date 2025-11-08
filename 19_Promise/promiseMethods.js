028d0ec3bb4870f98dcb62876264b09ad874fd37
Disha Sahu <sahu.disha21@gmail.com>
2025-11-09 00:44:39 +0530

    Promise Methods

---
+++ 19_Promise/promiseMethods.js
@@ -0,0 +1,25 @@
+let p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Promise 1')
+    }, 1800000);
+})
+
+let p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        reject('Promise 2')
+    }, 2000);
+})
+
+let p3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Promise 3')
+    }, 1500);
+})
+
+let promise = Promise.all([p1, p2, p3]) // will be resolved if all the promise all resolved
+// let promise = Promise.allSettled([p1, p2, p3]) //will be resolved once all are settled with resolved or rejected
+// let promise = Promise.race([p1, p2, p3]) //will return the promise which ever is settled first either resolved or rejected
+// let promise = Promise.any([p1, p2, p3]) //will return the promise which is resolved first
+// let promise = Promise.resolve('This is Promise') //create a new resolved promise
+// let promise = Promise.reject('Promise is rejected') //Creates a new rejected promise for the provided reason
+promise.then((value) => { console.log(value) })
